b2c3a41ac56784fb62ce02c00db6fefef0d9be08
Adam O'Neil <adamosoftware@gmail.com>
2018-12-28 11:09:40 -0500

    some working tests

--- Tests/AllTests.cs
+++ Tests/AllTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tests.Models;
 
 namespace Tests
 {
@@ -9,7 +10,7 @@ namespace Tests
 		private MySession _session = new MySession("sampleUser");
 
 		[TestMethod]
-		public void ShouldSaveSimpleValue()
+		public void SaveSimpleValue()
 		{
 			const string value = "hello";
 
@@ -18,6 +19,32 @@ namespace Tests
 			string greeting = _session.GetAsync<string>("greeting").Result;
 
 			Assert.AreEqual(value, greeting);
+		}
+
+		[TestMethod]
+		public void SaveComplexType()
+		{
+			const string rowKey = "complexType";
+
+			var sample = new SampleType()
+			{
+				OrderId = "KF34223",
+				Description = "whatever is this description",
+				Flag = true,
+				DateValue = new DateTime(2019, 1, 1),
+				Items = new NestedType[]
+				{
+					new NestedType() { ItemName = "thingus", Quantity = 34m, UnitPrice = 12.2m },
+					new NestedType() { ItemName = "globulor", Quantity = 78m, UnitPrice = 8.23m },
+					new NestedType() { ItemName = "prelcen", Quantity = 3.2m, UnitPrice = 34.9m }
+				}
+			};
+
+			_session.SetAsync(rowKey, sample).Wait();
+
+			var getSample = _session.GetAsync<SampleType>(rowKey).Result;
+
+			Assert.AreEqual(sample, getSample);
 		}
 	}
 }
---
+++ Tests/Models/SampleType.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Linq;
+
+namespace Tests.Models
+{
+	public class SampleType
+	{
+		public string OrderId { get; set; }
+		public string Description { get; set; }
+		public bool Flag { get; set; }
+		public DateTime DateValue { get; set; }
+		public NestedType[] Items { get; set; }
+
+		public override bool Equals(object obj)
+		{
+			var test = obj as SampleType;
+			if (test != null)
+			{
+				return
+					test.OrderId.Equals(OrderId) &&
+					test.Description.Equals(Description) &&
+					test.Flag.Equals(Flag) &&
+					test.DateValue.Equals(DateValue) &&
+					test.Items.SequenceEqual(Items);
+			}
+
+			return false;
+		}
+
+		public override int GetHashCode()
+		{
+			return base.GetHashCode();
+		}
+	}
+
+	public class NestedType
+	{
+		public string ItemName { get; set; }
+		public decimal Quantity { get; set; }
+		public decimal UnitPrice { get; set; }
+		public decimal ExtPrice { get { return Quantity * UnitPrice; } }
+
+		public override bool Equals(object obj)
+		{
+			var test = obj as NestedType;
+			if (test != null)
+			{
+				return
+					test.ItemName.Equals(ItemName) &&
+					test.Quantity.Equals(Quantity) &&
+					test.UnitPrice.Equals(UnitPrice);					
+			}
+
+			return false;
+		}
+
+		public override int GetHashCode()
+		{
+			return base.GetHashCode();
+		}
+	}
+}
