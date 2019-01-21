3987c713b46d58aedfd67830441212fa7babe222
Adam O'Neil <adamosoftware@gmail.com>
2019-01-20 19:25:34 -0500

    removed some obsolete hider methods

--- SessionTableStorage.Library/Interfaces/ITimedCacheableStorage.cs
+++ SessionTableStorage.Library/Interfaces/ITimedCacheableStorage.cs
@@ -5,12 +5,8 @@ using SessionTableStorage.Library.Interfaces;
 namespace SessionTableStorage.Library
 {
 	public interface ITimedCacheableStorage
-	{
-		T Get<T>(string rowKey, T defaultValue = default(T));
-		Task<T> GetAsync<T>(string rowKey, T defaultValue = default(T));
-		Task<T> GetAsync<T>(string rowKey, Func<Task<T>> update, T defaultValue = default(T)) where T : ITimedCacheable;
-		void Set(string rowKey, object data, Func<object, string> serializer = null);
-		Task SetAsync(string rowKey, object data, Func<object, string> serializer = null);
+	{				
+		Task<T> GetAsync<T>(string rowKey, Func<Task<T>> update, T defaultValue = default(T)) where T : ITimedCacheable;				
 		Task SetAsync<T>(string rowKey, T data, Func<object, string> serializer = null) where T : ITimedCacheable;
 	}
 }
--- SessionTableStorage.Library/TimedCacheableStorage.cs
+++ SessionTableStorage.Library/TimedCacheableStorage.cs
@@ -14,30 +14,6 @@ namespace SessionTableStorage.Library
 		{
 		}
 
-		#region hide base methods
-		// these methods don't use the proper cache logic, so they should not be used
-
-		public new async Task SetAsync(string rowKey, object data, Func<object, string> serializer = null)
-		{
-			throw new InvalidOperationException("To guarantee correct cache update, please use the overload that accepts a generic T argument.");
-		}
-
-		public new void Set(string rowKey, object data, Func<object, string> serializer = null)
-		{
-			throw new InvalidOperationException("To guarantee correct cache update, please use the overload that accepts a generic T argument.");
-		}
-
-		public new async Task<T> GetAsync<T>(string rowKey, T defaultValue = default(T))
-		{
-			throw new InvalidOperationException("To guarantee data freshness, please use the overload that accepts a Func<Task<T>>");
-		}
-
-		public new T Get<T>(string rowKey, T defaultValue = default(T))
-		{
-			throw new InvalidOperationException("To guarantee data freshness, please use the overload that accepts a Func<T>");
-		}
-		#endregion
-
 		public async Task<T> GetAsync<T>(string rowKey, Func<Task<T>> update, T defaultValue = default(T)) where T : ITimedCacheable
 		{
 			T result = default(T);
